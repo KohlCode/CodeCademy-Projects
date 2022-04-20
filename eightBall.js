61d39d133aec788d88ff76ea7dcb818f6487363c
Sam <55670463+KohlCode@users.noreply.github.com>
2022-04-20 12:11:05 -0700

    Create eightBall.js

---
+++ eightBall.js
@@ -0,0 +1,39 @@
+let userName = '';
+let defaultName = userName || 'Stranger'
+let userQuestion =  'Will I ever fall in love?';
+let randomNumber = Math.floor(Math.random() * 8)
+let eightBall = '';
+
+userName ? console.log(`Hello, ${userName}`) : console.log(`Hello, ${defaultName}`)
+
+console.log(`${defaultName} asked: 'Will I ever fall in love?`)
+
+switch (randomNumber){
+  case 0:
+    console.log('It is certain')
+    break;
+  case 1:
+    console.log('It is decidedly so')
+    break;
+  case 2:
+    console.log('Reply hazy try again')
+    break;
+  case 3:
+    console.log('Cannot predict now')
+    break;
+  case 4:
+    console.log('Do not count on it')
+    break;
+  case 5:
+    console.log('My sources say no')
+    break;
+  case 6:
+    console.log('Outlook not so good')
+    break;
+  case 7:
+    console.log('Signs point to yes')
+    break;
+  deault:
+    console.log('Invalid Input')
+    break;
+}
